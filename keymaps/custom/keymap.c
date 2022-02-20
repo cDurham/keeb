c2190aec53631553179d39503b80899e5796be73
Chris Durham <{ID}+{username}@users.noreply.github.com>
2022-02-20 16:20:59 -0700

    adding \

--- keymaps/custom/keymap.c
+++ keymaps/custom/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  * ctlesc - left control when held, esc when tapped
  */
-[_QWERTY] = LAYOUT_ortho_4x12(
+[_QWERTY] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, \
   LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   LSFT_DEL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, SFTENT, \
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  * ctlesc - left control when held, esc when tapped
  */
-[_QGMLWB] = LAYOUT_ortho_4x12(
+[_QGMLWB] = LAYOUT_ortho_4x12( \
    KC_TAB,  KC_Q,    KC_G,    KC_M,    KC_L,    KC_W,    KC_B,   KC_Y,    KC_U,    KC_V,    KC_SCLN, KC_BSPC, \
    KC_ESC,  KC_D,    ALT_S,   CTRL_T,  SFT_N,   KC_R,    KC_I,   SFT_A,   CTL_E,   ALT_O,   KC_H,    KC_QUOT, \
    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_F,    KC_J,    KC_K,   KC_P,    KC_COMM, KC_DOT,  KC_SLSH, SFTENT, \
